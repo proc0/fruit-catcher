76809f0da4819ee4bafa573055b3fb8e409dea87
proc0 <alanvx@gmail.com>
2025-02-14 06:26:13 -0800

    adding main file

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,77 +1,253 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
--- Copyright (c) 2020-2024 Jeffery Myers
---
---This software is provided "as-is", without any express or implied warranty. In no event 
---will the authors be held liable for any damages arising from the use of this software.
-
---Permission is granted to anyone to use this software for any purpose, including commercial 
---applications, and to alter it and redistribute it freely, subject to the following restrictions:
-
---  1. The origin of this software must not be misrepresented; you must not claim that you 
---  wrote the original software. If you use this software in a product, an acknowledgment 
---  in the product documentation would be appreciated but is not required.
---
---  2. Altered source versions must be plainly marked as such, and must not be misrepresented
---  as being the original software.
---
---  3. This notice may not be removed or altered from any source distribution.
-
-*/
+#include <stdlib.h>
+#include <time.h>
 
 #include "raylib.h"
 #include "raymath.h"
 
-#include "game.h"   // an external header in this project
-#include "lib.h"	// an external header in the static lib project
+#include "game.h"
+#include "lib.h"
 
+#define APPLE_SOURCE_WIDTH 75
+#define APPLE_SOURCE_HEIGHT 75
+#define APPLE_MAX_COUNT 8
+#define APPLE_SOURCE_RECTANGLE CLITERAL(Rectangle){0, 0, APPLE_SOURCE_WIDTH, APPLE_SOURCE_HEIGHT}
 
-void GameInit()
-{
-    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
-    InitWindow(InitalWidth, InitalHeight, "Example");
-    SetTargetFPS(144);
+#define BASKET_SOURCE_WIDTH 100
+#define BASKET_SOURCE_HEIGHT 100
+#define BASKET_SOURCE_RECTANGLE CLITERAL(Rectangle){0, 0, BASKET_SOURCE_WIDTH, BASKET_SOURCE_HEIGHT}
 
-    // load resources
+#define FALL_SPEED_MIN 150
+#define FALL_SPEED_MAX 250
+
+#define TIME_BETWEEN_APPLES 1.0f
+#define INITIAL_LIVES 3
+#define APPLE_SCORE 100
+
+#define GAME_OVER_TEXT "Game Over"
+#define RESTART_MESSAGE "Press R to restart"
+
+const int screenWidth = 800;
+const int screenHeight = 600;
+const Vector2 screenSize = { screenWidth, screenHeight };
+
+typedef struct Apple {
+    Vector2 position;
+    int fallSpeed;
+    bool _active;
+} Apple;
+
+typedef enum GameState {
+    PLAYING,
+    END
+} GameState;
+
+float _timeGameStarted;
+float _timeGameEnded;
+float _nextAppleTimer;
+float _basketXPosition;
+int _score;
+int _lives;
+
+GameState _state;
+Apple _apples[APPLE_MAX_COUNT];
+
+Texture2D _atlasApples;
+Texture2D _atlasBasket;
+
+void GameInit(void);
+void UpdateDrawFrame(void);
+void GameEnd(void);
+
+void SetAppleAt(int, Vector2, int);
+void UnsetAppleAt(int);
+
+void UpdateApples(void);
+void SpawnApple(void);
+void DrawApples(void);
+
+Vector2 GetBasketPosition(void);
+void DrawBasket(void);
+
+int main() {
+    
+  srand(time(0));
+  
+  InitWindow(screenWidth, screenHeight, "Fruit Catcher");
+  SetTargetFPS(60);
+  
+  _atlasApples = LoadTexture("resources/applesprites.png");
+  _atlasBasket = LoadTexture("resources/basket.png");
+  
+  GameInit();
+  
+  while (!WindowShouldClose()) {
+    UpdateDrawFrame();
+  }
+  
+  CloseWindow();
+  
+  return 0;
 }
 
-void GameCleanup()
-{
-    // unload resources
-
-    CloseWindow();
-}
-
-bool GameUpdate()
-{
-    return true;
-}
-
-void GameDraw()
-{
-    BeginDrawing();
-    ClearBackground(DARKGRAY);
-
-    DrawText("Hello Raylib!", 10, 10, 20, GetTextColor());
-
-    EndDrawing();
-}
-
-int main()
-{
-    GameInit();
-
-    while (!WindowShouldClose())
-    {
-        if (!GameUpdate())
-            break;
-
-        GameDraw();
+void SetAppleAt(int i, Vector2 position, int fallSpeed) {
+    if(i < 0 || i > APPLE_MAX_COUNT){
+        return;
     }
-    GameCleanup();
+    _apples[i].position = position;
+    _apples[i].fallSpeed = fallSpeed;
+    _apples[i]._active = true;
+}
 
-    return 0;
+void UnsetAppleAt(int i) {
+    if(i < 0 || i > APPLE_MAX_COUNT){
+        return;
+    }
+    
+    _apples[i]._active = false;
+}
+
+void GameInit(void) {
+    _state = PLAYING;
+    _timeGameStarted = GetTime();
+    _nextAppleTimer = TIME_BETWEEN_APPLES;
+    _lives = INITIAL_LIVES;
+    _score = 0;
+    
+    for(int i=0; i < APPLE_MAX_COUNT; i++){
+        UnsetAppleAt(i);
+    }
+}
+
+void DrawApples(void) {
+    for(int i=0; i < APPLE_MAX_COUNT; i++){
+       if(!_apples[i]._active){
+           continue;
+       }
+       Vector2 position = _apples[i].position;
+       position.x -= APPLE_SOURCE_WIDTH / 2;
+       position.y -= APPLE_SOURCE_HEIGHT / 2;
+       DrawTextureRec(_atlasApples, APPLE_SOURCE_RECTANGLE, position, WHITE);
+    }
+}
+
+Vector2 GetBasketPosition(void) {
+    
+    if(_state == PLAYING) {
+        _basketXPosition = GetMousePosition().x;
+    }
+    
+    return (Vector2){ _basketXPosition, screenHeight - (BASKET_SOURCE_HEIGHT * 0.75) };
+}
+
+void DrawBasket(void) {
+    Vector2 position = GetBasketPosition();
+    
+    position.x -= _atlasBasket.width / 2;
+    position.y -= _atlasBasket.height / 2;
+    
+    DrawTextureRec(_atlasBasket, BASKET_SOURCE_RECTANGLE, position, WHITE);
+}
+
+void UpdateApples(void) {
+    for(int i=0; i < APPLE_MAX_COUNT; i++){
+       if(!_apples[i]._active){
+           continue;
+       }
+        
+       if(_apples[i].position.y > screenHeight) {
+            UnsetAppleAt(i);
+            _lives--;
+            if(_lives <= 0) {
+                GameEnd();
+                return;
+            }
+            continue;
+       }
+       
+       Vector2 basketPosition = GetBasketPosition();
+       if(fabsf(_apples[i].position.x - basketPosition.x) < BASKET_SOURCE_WIDTH && fabsf(_apples[i].position.y - basketPosition.y) < BASKET_SOURCE_HEIGHT / 4 ) {
+           UnsetAppleAt(i);
+           _score++;
+           continue;
+       }
+       
+       float offset = _apples[i].fallSpeed * GetFrameTime();
+       _apples[i].position.y += offset;
+    }
+}
+
+void SpawnApple(void) {
+    int availableIndex = -1;
+    for(int i=0; i < APPLE_MAX_COUNT; i++){
+        if(!_apples[i]._active){
+           availableIndex = i;
+           break;
+        }
+    }
+    
+    if(availableIndex == -1) {
+        return;
+    }
+    
+    int fallSpeed = GetRandomValue(FALL_SPEED_MIN, FALL_SPEED_MAX);
+    int posX = GetRandomValue(APPLE_SOURCE_WIDTH / 2, screenWidth - (APPLE_SOURCE_WIDTH / 2));
+    
+    SetAppleAt(availableIndex, (Vector2){posX, -APPLE_SOURCE_HEIGHT}, fallSpeed);
+}
+
+void GameEnd(void) {
+    _state = END;
+    _timeGameEnded = GetTime();
+}
+
+void UpdateDrawFrame(void) {
+    
+    if(_state == END) {
+        if(IsKeyPressed(KEY_R)){
+            GameInit();
+        }
+    }
+    
+    if(_state == PLAYING) {
+        UpdateApples();
+        
+        _nextAppleTimer -= GetFrameTime();
+        if(_nextAppleTimer <= 0) {
+            _nextAppleTimer = TIME_BETWEEN_APPLES;
+            SpawnApple();
+        }
+    }
+    
+    //if (win condition){
+    //    GameEnd();
+    //}
+
+    // Draw Game
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawBasket();
+    DrawApples();
+
+    if(_state == END) {
+        const char* gameOver = GAME_OVER_TEXT;
+        const char* restartMessage = RESTART_MESSAGE;
+        
+        DrawRectangle(0, 0, screenWidth, screenHeight, Fade(WHITE, 0.8f));
+        DrawText(gameOver, screenWidth / 2 - MeasureText(gameOver, 40) / 2, screenHeight / 2 - 10, 40, BLACK);
+        DrawText(restartMessage, screenWidth / 2 - MeasureText(restartMessage, 20) / 2, screenHeight * 0.75f, 20, DARKGRAY);
+        
+        const char* scoreText = TextFormat("SCORE: %d", _score);
+        DrawText(scoreText, screenWidth / 2 - MeasureText(scoreText, 32) / 2, screenHeight * 0.25f, 32, BLACK);
+        
+        int totalMinutes = (int)(_timeGameEnded - _timeGameStarted) / 60;
+        int totalSeconds = (int)(_timeGameEnded - _timeGameStarted) % 60;
+        DrawText(TextFormat("Time Played: %d minutes and %d seconds", totalMinutes, totalSeconds), 20, screenHeight - 40, 20, DARKGRAY);
+    } else {
+        const char* scoreText = TextFormat("SCORE: %d", _score);
+        const char* livesText = TextFormat("LIVES: %d", _lives);
+        DrawText(scoreText, 10, 10, 32, BLACK);
+        DrawText(livesText, 10, 35, 24, DARKGRAY);
+    }
+    
+    EndDrawing();
 }
