f7a3812b8f5241cb984866d5c873aa43502bfa1b
proc0 <alanvx@gmail.com>
2025-02-20 07:53:02 -0800

    cleaning up display functions

--- game/src/display.cpp
+++ game/src/display.cpp
@@ -6,25 +6,27 @@ const char *gameOver = TEXT_GAME_OVER;
 const char *restartMessage = TEXT_RESTART;
 const char *textTimePlayed = TEXT_TIME_PLAYED;
 
-const int TITLE_FONT_SIZE = 40;
-const int SUBTITLE_FONT_SIZE = 20;
-const int SCORE_FONT_SIZE = 32;
+static constexpr int TITLE_FONT_SIZE = 40;
+static constexpr int SUBTITLE_FONT_SIZE = 20;
+static constexpr int SCORE_FONT_SIZE = 32;
+static constexpr int SCREEN_HALF_WIDTH = SCREEN_WIDTH/2;
 
 void Display::Ending(int lives, int score, float timeEnd, float timeStart) {
-    const int screenWidthHalf = SCREEN_WIDTH/2;
-    const int centerGameOver = screenWidthHalf - MeasureText(gameOver, TITLE_FONT_SIZE)/2;
-    const int centerRestartMessage = screenWidthHalf - MeasureText(restartMessage, SUBTITLE_FONT_SIZE)/2;
-    const int verticalAlignTitle = SCREEN_HEIGHT/2 - 10;
-    const int verticalAlignSubtitle = SCREEN_HEIGHT * 0.75f;
     DrawRectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, Fade(WHITE, 0.8f));
-    DrawText(gameOver, centerGameOver, verticalAlignTitle, TITLE_FONT_SIZE, BLACK);
-    DrawText(restartMessage, centerRestartMessage, verticalAlignSubtitle, SUBTITLE_FONT_SIZE, DARKGRAY);
-    
+
     const char *scoreText = TextFormat(textScore, score);
-    const int centerScoreText = screenWidthHalf - MeasureText(scoreText, SCORE_FONT_SIZE)/2;
+    const int centerScoreText = SCREEN_HALF_WIDTH - MeasureText(scoreText, SCORE_FONT_SIZE)/2;
     const int verticalAlignScore = SCREEN_HEIGHT * 0.25f;
     DrawText(scoreText, centerScoreText, verticalAlignScore, SCORE_FONT_SIZE, GetTextColor());
     
+    const int centerGameOver = SCREEN_HALF_WIDTH - MeasureText(gameOver, TITLE_FONT_SIZE)/2;
+    const int verticalAlignTitle = SCREEN_HEIGHT/2 - 10;
+    DrawText(gameOver, centerGameOver, verticalAlignTitle, TITLE_FONT_SIZE, BLACK);
+
+    const int centerRestartMessage = SCREEN_HALF_WIDTH - MeasureText(restartMessage, SUBTITLE_FONT_SIZE)/2;
+    const int verticalAlignSubtitle = SCREEN_HEIGHT * 0.75f;
+    DrawText(restartMessage, centerRestartMessage, verticalAlignSubtitle, SUBTITLE_FONT_SIZE, DARKGRAY);
+
     int totalMinutes = (int)(timeEnd - timeStart)/60;
     int totalSeconds = (int)(timeEnd - timeStart) % 60;
     const char *totalTime = TextFormat(textTimePlayed, totalMinutes, totalSeconds);
