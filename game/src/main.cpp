19aa602cb1ddc6f86ee57975072d0bac2b867ed0
proc0 <alanvx@gmail.com>
2025-04-24 01:42:20 -0700

    updating todos

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -2,16 +2,14 @@
 
 #define GAME_CONFIG_URI "game/FruitCatcher.ini"
 
-//TODO: add timer
-//TODO: instead of lives, make the time tick down for every fruit that is missed... or create two modes, or blend the modes?
 
-//TODO: update game name
 //TODO: make bucket into a transparent jar, every fruit turns into jam at the bottom, layers build up and clear when it gets to the top.
-//TODO: add progressive difficulty, starting with small fruit and more fruits are added faster -- LEVELS
-//TODO: add timer (and LEVELS)
 //TODO: spice up the game over screen
-//TODO: add animations when scoring and losing lives in HUD
+//TODO: add eggs that add one life point
+//TODO: add progressive difficulty, starting with small fruit and more fruits are added faster -- LEVELS
 //TODO: add satisfying sound when fruit is caught, add some other secondary effects (cartoon lines)
+//TODO: update game name
+
 int main() {
 
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, TEXT_GAME_TITLE);
